fe9191a86dba3bbdcc9875ad4d00af9d9541ed66
valadyslavP <danillppiont106@gmail.com>
2021-09-26 15:40:27 +0300

    react gsap combination

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import React, { useRef, useEffect, useState } from 'react';
+import { gsap } from "gsap";
+import logo from './images/img_react-gsap.png';
+import './App.css';
+
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
+
+const sections = [
+  {
+    title: 'Architecto aliquam', 
+    subtitle: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. At, ea.'
+  },
+  {
+    title: 'Ceritatis placeat', 
+    subtitle: 'Dignissimos placeat cupiditate perferendis eaque praesentium similique officia dolore?'
+  },
+  {
+    title: 'Vitae voluptates', 
+    subtitle: 'In ullam et nulla repudiandae praesentium, laboriosam quas tempore fuga asperiores eveniet amet.'
+  }
+];
+
+const App = () => {
+
+  const [background, setBackground] = useState('#262626');
+  const headerRef = useRef(null);
+
+  const revealRefs = useRef([]);
+  revealRefs.current = [];
+
+  const toggleBackground = () => {
+    const color = background !== '#262626' ? '#262626' : '#1b4943';
+    setBackground(color);
+  }
+
+  useEffect(() => {
+
+    gsap.to(headerRef.current, { backgroundColor: background, duration: 1,  ease: 'none' });
+
+  }, [background]);
+
+  useEffect(() => {
+    
+    gsap.from(headerRef.current, {
+      autoAlpha: 0, 
+      ease: 'none',
+      delay: 1
+    });
+
+    revealRefs.current.forEach((el, index) => {
+        
+      gsap.fromTo(el, {
+        autoAlpha: 0
+      }, {
+        duration: 1, 
+        autoAlpha: 1,
+        ease: 'none',
+        scrollTrigger: {
+          id: `section-${index+1}`,
+          trigger: el,
+          start: 'top center+=300',
+          toggleActions: 'play none none reverse'
+        }
+      });
+
+    });
+
+  }, []);
+
+  const addToRefs = el => {
+    if (el && !revealRefs.current.includes(el)) {
+        revealRefs.current.push(el);
+    }
+  };
+
+  return (
+    <div className="App">
+      <header ref={headerRef} className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={() => toggleBackground()}>Change background</button>
+        <p>
+          Scroll down to see sections being revealed by ScrollTrigger.
+        </p>
+      </header>
+      <main className="App-main">
+        {
+          sections.map(({title, subtitle}) => (
+            <div className="App-section" key={title} ref={addToRefs}>
+              <h2>{title}</h2>
+              <p>{subtitle}</p>
+            </div>
+          ))
+        }
+      </main>
+    </div>
+  );
+}
+
+export default App;
